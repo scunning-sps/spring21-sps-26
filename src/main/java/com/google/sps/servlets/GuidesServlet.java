3dc6fe18385f273ab1a76e3bae7f2b8320533eeb
Dario Martinez <gusdamtzr@gmail.com>
2021-03-26 01:50:47 +0000

    Adds doPost method to GuidesServlet. Implements functions to test that post works in the front end

--- src/main/java/com/google/sps/servlets/GuidesServlet.java
+++ src/main/java/com/google/sps/servlets/GuidesServlet.java
@@ -13,16 +13,42 @@ import com.google.gson.Gson;
 @WebServlet("/guides")
 public class GuidesServlet extends HttpServlet{
     static final long serialVersionUID = 0;
+    private final ArrayList<String> guides = new ArrayList<>();
+    private int lastId = 0;
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        ArrayList<Object> list = new ArrayList<>();
-
         Gson gson = new Gson();
 
         response.setContentType("application/json");
-        response.getWriter().print(gson.toJson(list));        
-        
+        response.getWriter().print(gson.toJson(guides));        
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+        String content = request.getParameter("content");
+        int id = ++lastId;
+        long timestamp = System.currentTimeMillis();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("{ \"title\": \"");
+        sb.append(title);
+        sb.append("\", \"description\": \"");
+        sb.append(description);
+        sb.append("\", \"content\": \"");
+        sb.append(content);
+        sb.append("\", \"id\": \"");
+        sb.append(id);
+        sb.append("\", \"timestamp\": ");
+        sb.append(timestamp);
+        sb.append("}");
+
+        response.setContentType("application/json");
+        response.getWriter().print(sb.toString());
+
+        guides.add(sb.toString());
     }
 
     
---
+++ src/main/webapp/testAPI.js
@@ -0,0 +1,26 @@
+async function postGuide({ title, description, content }){
+    let response = await fetch(`/guides?title=${ title }&description=${ description }&content=${ content }` , {
+        method: 'POST'
+    });
+
+    if(response.status != 200){
+        throw Error('Something went wrong with the request.');
+    }
+
+    let guide = await response.json();
+    console.log(guide);
+}
+
+function testPostGuides(){
+    let exampleTitle = 'Example title ';
+    let exampleDescription = 'Example description ';
+    let exampleContent = 'Example content ';
+
+    for(let i = 1; i < 11;i++){
+        postGuide({
+            title: exampleTitle + i,
+            description: exampleDescription + i,
+            content: exampleContent + i,
+        });
+    }
+}
